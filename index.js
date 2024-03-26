5f9a013ffa79fd56ff1265448f103777c2ae3886
Darian King <kingransom9411@gmail.com>
2024-03-27 02:53:01 +0900

    modified for single server

--- index.js
+++ index.js
@@ -25,77 +25,146 @@ app.use(cors({ origin: '*' }));
 
 const port = process.env.SERVER_PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get("/api", (req, res) => {
     // res.sendFile(path.join(__dirname + '/dist/index.html'));
-    res.status(200).send({success: true, msg: 'Hello Commune fam, this is a message from bot marketplace server.'})
-});
-
-const getApplicationInfo = async (token, appId)=>{
-    const response = await axios.get(`https://discord.com/api/v9/applications/${appId}`, {
+    res
+      .status(200)
+      .send({
+        success: true,
+        msg: "Hello Commune fam, this is a message from bot marketplace server.",
+      });
+  });
+  
+  // app.get("/hello", (_, res) => {
+  //   res.send("Hello Vite + React + TypeScript!");
+  // });
+  
+  const getApplicationInfo = async (token, appId) => {
+    const response = await axios.get(
+      `https://discord.com/api/v9/applications/${appId}`,
+      {
         headers: {
-            authorization: `Bot ${token}`
-        }
-    });
-
+          authorization: `Bot ${token}`,
+        },
+      }
+    );
+  
     return response.data;
-}
-
-app.get('/bot', async (req, res)=> {
-    const info = await getApplicationInfo(req.query.token, req.query.appId);
-    res.status(200).send(info);
-});
-
-app.post('/bot/upvote', async(req, res)=> {
-    const { botId } = req.body;
-    botsDB.findOne({clientID: botId}, (err, doc)=> {
+  };
+  
+  app.get("/api/bot", async (req, res) => {
+    try {
+      const info = await getApplicationInfo(req.query.token, req.query.appId);
+      res.status(200).send(info);
+    } catch (err) {
+      res.status(500).send({ success: false, msg: err });
+    }
+  });
+  
+  app.post("/api/bot/upvote", async (req, res) => {
+    try {
+      const { botId } = req.body;
+      botsDB.findOne({ clientID: botId }, (err, doc) => {
         if (err) {
-            res.status(500).send({success: false, msg: err})
+          res.status(500).send({ success: false, msg: err });
         } else {
-            const vote = doc['vote'];
-            doc[vote] = vote + 1;
-            botsDB.update({clientID: botId}, {$set: {vote: vote + 1}}, {},  (error, numReplaced)=> {
-                if (error) {
-                    res.status(500).send({success: false, msg: error})
-                } else {
-                    res.status(200).send({success: true});
-                }
-            });
+          const vote = doc["vote"];
+          doc[vote] = vote + 1;
+          botsDB.update(
+            { clientID: botId },
+            { $set: { vote: vote + 1 } },
+            {},
+            (error, numReplaced) => {
+              if (error) {
+                res.status(500).send({ success: false, msg: error });
+              } else {
+                res.status(200).send({ success: true });
+              }
+            }
+          );
         }
-    })
-})
-
-app.get('/bots', (req, res)=>{
-    console.log(req.query)
-    const {search, user} = req.query;
-    let filter = {}
-
-    if (search === 'mine')
+      });
+    } catch (err) {
+      res.status(500).send({ success: false, msg: err });
+    }
+  });
+  
+  app.get("/api/bots", (req, res) => {
+    try {
+      console.log(req.query);
+      const { search, user } = req.query;
+      let filter = {};
+  
+      if (search === "mine")
         filter = {
-            "owner.id" : user
-        }
-    botsDB.find(filter, (err, docs)=> {
+          "owner.id": user,
+        };
+      botsDB.find(filter, (err, docs) => {
         if (err) {
-            res.status(500).send({success: false, msg: err});
+          res.status(500).send({ success: false, msg: err });
         } else {
-            res.status(200).send({success: true, bots: docs});
+          res.status(200).send({ success: true, bots: docs });
         }
-    });
-});
-
-app.post('/bot/add', async (req, res) => {
-    const { botToken, clientID, shortDesc, desc, cmdPrefix, website, inviteLink, serverInvite} = req.body;
-    const { name, icon, description: appDesc, type, cover_image: coverImage, bot, summary, tags, owner } = await getApplicationInfo(botToken, clientID);
-    
-    botsDB.insert({
-        botToken, clientID, shortDesc, desc, cmdPrefix, website, inviteLink, serverInvite, 
-        name, icon, appDesc, type, coverImage, bot, summary, tags, owner, vote: 0
-    }, (err)=> {
-        if (err)
-            res.status(200).send({success: false, msg: err});
-        else
-            res.status(200).send({success: true, msg: 'Saved'});
-    });
-});
+      });
+    } catch (err) {
+      res.status(500).send({ success: false, msg: err });
+    }
+  });
+  
+  app.post("/api/bot/add", async (req, res) => {
+    try {
+      const {
+        botToken,
+        clientID,
+        shortDesc,
+        desc,
+        cmdPrefix,
+        website,
+        inviteLink,
+        serverInvite,
+      } = req.body;
+      const {
+        name,
+        icon,
+        description: appDesc,
+        type,
+        cover_image: coverImage,
+        bot,
+        summary,
+        tags,
+        owner,
+      } = await getApplicationInfo(botToken, clientID);
+  
+      botsDB.insert(
+        {
+          botToken,
+          clientID,
+          shortDesc,
+          desc,
+          cmdPrefix,
+          website,
+          inviteLink,
+          serverInvite,
+          name,
+          icon,
+          appDesc,
+          type,
+          coverImage,
+          bot,
+          summary,
+          tags,
+          owner,
+          vote: 0,
+        },
+        (err) => {
+          if (err) res.status(200).send({ success: false, msg: err });
+          else res.status(200).send({ success: true, msg: "Saved" });
+        }
+      );
+    } catch (err) {
+      res.status(500).send({ success: false, msg: err });
+    }
+  });
 
 app.listen(port, function () { 
     console.log(`App listening! Port: ${port}`);
